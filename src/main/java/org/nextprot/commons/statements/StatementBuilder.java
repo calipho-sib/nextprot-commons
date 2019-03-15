52bf4041bb43b2d780b7982b747f74600fb17c4e
fnikitin <frederic.nikitin@isb-sib.ch>
2019-03-15 10:55:56 +0100

    Move method into builder and remove useless methods

--- src/main/java/org/nextprot/commons/statements/StatementBuilder.java
+++ src/main/java/org/nextprot/commons/statements/StatementBuilder.java
@@ -1,17 +1,21 @@
 package org.nextprot.commons.statements;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
+import org.nextprot.commons.algo.MD5Algo;
 import org.nextprot.commons.constants.QualityQualifier;
 import org.nextprot.commons.statements.constants.AnnotationType;
 import org.nextprot.commons.utils.StringUtils;
 
-import static org.nextprot.commons.statements.StatementField.*;
+import static org.nextprot.commons.statements.PredefinedStatementField.*;
 
 public class StatementBuilder {
 
@@ -22,31 +26,22 @@ public class StatementBuilder {
 		return sb;
 	}
 
+	// Note: Used in bed
 	public static StatementBuilder createFromExistingStatement(Statement s) {
 		StatementBuilder sb = new StatementBuilder();
 		sb.addMap(s);
 		return sb;
 	}
 
-	public StatementBuilder removeField(StatementField statementField) {
-		this.keyValues.remove(statementField.name());
-		return this;
-	}
-	
 	public StatementBuilder addField(StatementField statementField, String statementValue) {
 		this.keyValues.put(statementField.name(), statementValue);
 		return this;
 	}
 
-	public StatementBuilder addTargetIsoformsField(TargetIsoformSet targetIsoforms) {
-		this.keyValues.put(StatementField.TARGET_ISOFORMS.name(), targetIsoforms.serializeToJsonString());
-		return this;
-	}
-
 	public StatementBuilder addSubjects(Collection<Statement> statements) {
 
-		Set<String> sortedStatementIds = new TreeSet<String>();
-		Set<String> sortedAnnotationIds = new TreeSet<String>();
+		Set<String> sortedStatementIds = new TreeSet<>();
+		Set<String> sortedAnnotationIds = new TreeSet<>();
 
 		Iterator<Statement> statementsIt = statements.iterator();
 
@@ -68,12 +63,13 @@ public class StatementBuilder {
 		addField(SUBJECT_STATEMENT_IDS, subjectStatemendIds);
 		
 		if(!subjectAnnotationIds.isEmpty()){
-			addField(StatementField.SUBJECT_ANNOTATION_IDS, subjectAnnotationIds);
+			addField(PredefinedStatementField.SUBJECT_ANNOTATION_IDS, subjectAnnotationIds);
 		}
 
 		return this;
 	}
 
+	// Note: Used in bed
 	public StatementBuilder addObject(Statement statement) {
 		addField(OBJECT_STATEMENT_IDS, statement.getStatementId());
 		if(statement.getAnnotationId() != null){
@@ -82,18 +78,6 @@ public class StatementBuilder {
 		}
 		return this;
 	}
-
-	public Statement build() {
-		Statement rs = new Statement(keyValues);
-		rs.putValue(StatementField.STATEMENT_ID, StatementUtil.computeAndGetAnnotationId(rs, AnnotationType.STATEMENT));
-		return rs;
-	}
-
-    public Statement buildWithAnnotationHash() {
-        Statement rs = build();
-        rs.putValue(StatementField.ANNOTATION_ID, StatementUtil.computeAndGetAnnotationId(rs, AnnotationType.ENTRY));
-        return rs;
-    }
 
 	public StatementBuilder addMap(Map<String, String> map) {
 		keyValues.putAll(map);
@@ -107,6 +91,7 @@ public class StatementBuilder {
 		return this;
 	}
 
+	// Note: Used in bed
 	public StatementBuilder addCvTerm(String cvTermAccession, String cvTermName, String cvTerminology) {
 		addField(ANNOT_CV_TERM_ACCESSION, cvTermAccession);
 		addField(ANNOT_CV_TERM_NAME, cvTermName);
@@ -115,7 +100,7 @@ public class StatementBuilder {
 	}
 
 	public StatementBuilder addQuality(QualityQualifier quality) {
-		addField(StatementField.EVIDENCE_QUALITY, quality.name());
+		addField(PredefinedStatementField.EVIDENCE_QUALITY, quality.name());
 		return this;
 	}
 
@@ -129,15 +114,10 @@ public class StatementBuilder {
 
 	public StatementBuilder addDebugInfo(String info) {
 		addField(DEBUG_INFO, info);
-		/*		if(!this.keyValues.containsKey(DEBUG_INFO.name())){
-			addField(DEBUG_INFO, info);
-		}else {
-			String msg = this.keyValues.get(DEBUG_INFO.name());
-			addField(DEBUG_INFO, msg + "; " + info);
-		}*/
 		return this;
 	}
 
+	// Note: Used in bed
 	public StatementBuilder addVariantInfo(String annotationCategory, String firstPosition, String lastPosition, String variationOrigin, String variationVariation) {
 
 		if(annotationCategory == null || !(annotationCategory.equals("variant") || annotationCategory.equals("mutagenesis"))) {
@@ -155,10 +135,53 @@ public class StatementBuilder {
 
 	}
 
-	public StatementBuilder addLocationFields(String locationBegin, String locationEnd) {
-		addField(LOCATION_BEGIN, locationBegin);
-		addField(LOCATION_END, locationEnd);
-		return this;
+	public Statement build() {
+		Statement rs = new Statement(keyValues);
+		rs.putValue(PredefinedStatementField.STATEMENT_ID, computeUniqueKey(rs, AnnotationType.STATEMENT));
+		return rs;
 	}
 
+	public Statement buildWithAnnotationHash() {
+		Statement rs = build();
+		rs.putValue(PredefinedStatementField.ANNOTATION_ID, computeUniqueKey(rs, AnnotationType.ENTRY));
+		return rs;
+	}
+
+	/**
+	 * This method compute a MD5 unique key based on the combination of specified statement fields
+	 * @param statement the statement to compute unique key on
+	 * @param type see with pam
+	 * @return a unique key
+	 */
+	public static String computeUniqueKey(Statement statement, AnnotationType type) {
+
+		// Filter fields which are used to compute unicity
+		Set<StatementField> unicityFields = new HashSet<>();
+		StatementField[] fields = PredefinedStatementField.values();
+		for (StatementField field : fields) {
+			// According with
+			// https://calipho.isb-sib.ch/wiki/display/cal/Raw+statements+specifications
+
+			if (type.equals(AnnotationType.ENTRY)) {
+				if (field.isPartOfUnicityKey()) {
+					unicityFields.add(field);
+				}
+			}
+			else if (type.equals(AnnotationType.STATEMENT)) { // All fields for the statement
+				if (!field.equals(PredefinedStatementField.STATEMENT_ID)) {
+					unicityFields.add(field);
+				}
+			}
+		}
+
+		List<String> contentItems = new ArrayList<>();
+		for (StatementField unicityField : unicityFields) {
+			String value = statement.getValue(unicityField);
+			if (value != null) {
+				contentItems.add(value);
+			}
+		}
+
+		return MD5Algo.computeMD5(String.join("", contentItems));
+	}
 }
