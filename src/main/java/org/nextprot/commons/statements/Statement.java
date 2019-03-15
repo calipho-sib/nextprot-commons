5e3c4a88c35dda8b67c5171614919b38b0f3a7db
fnikitin <frederic.nikitin@isb-sib.ch>
2019-03-15 11:47:59 +0100

    Adapt client to enum renaming

--- src/main/java/org/nextprot/commons/statements/Statement.java
+++ src/main/java/org/nextprot/commons/statements/Statement.java
@@ -5,24 +5,19 @@ import java.util.TreeMap;
 
 /**
  * DO NOT ADD public setters on this class.
- * A StatementID is computed based on the fields when build() is invoked
- * 
- * @author Daniel Teixeira http://github.com/ddtxra
- *
  */
 public class Statement extends TreeMap<String, String>{
 
 	private static final long serialVersionUID = -4723168061980820149L;
 	private boolean isProcessed = false;
 	
-	//Needed for serialization in Play?
 	public Statement() {
 		super();
 	}
 
 	// Keep the constructor package protected, so it enforces the use of the Builder
 	Statement(Map<String, String> map) {
-		super(new TreeMap<String, String>(map));
+		super(new TreeMap<>(map));
 	}
 
 	public String getValue(StatementField field) {
@@ -30,41 +25,37 @@ public class Statement extends TreeMap<String, String>{
 	}
 
 	public String getDebugInfo() {
-		return get(StatementField.DEBUG_INFO.name());
+		return get(PredefinedStatementField.DEBUG_INFO.name());
 	}
 
 	String putValue(StatementField field, String value) {
 		return put(field.name(), value);
 	}
-	
-/*	void computeAndSetAnnotationIds(AnnotationType annotationType){
-		putValue(StatementField.ANNOTATION_ID, StatementUtil.computeAndGetAnnotationId(this,annotationType));
-	}*/
 
 	public String getSubjectStatementIds() {
-		return getValue(StatementField.SUBJECT_STATEMENT_IDS);
+		return getValue(PredefinedStatementField.SUBJECT_STATEMENT_IDS);
 	}
 	
 	public String[] getSubjectStatementIdsArray() {
-		String subjects = getValue(StatementField.SUBJECT_STATEMENT_IDS);
+		String subjects = getValue(PredefinedStatementField.SUBJECT_STATEMENT_IDS);
 		if(subjects == null) return null;
 		else return subjects.split(",");
 	}
 	
 	public String getStatementId() {
-		return this.getValue(StatementField.STATEMENT_ID);
+		return this.getValue(PredefinedStatementField.STATEMENT_ID);
 	}
 	
 	public String getAnnotationId() {
-		return this.getValue(StatementField.ANNOTATION_ID);
+		return this.getValue(PredefinedStatementField.ANNOTATION_ID);
 	}
 	
 	public String getObjectStatementId() {
-		return getValue(StatementField.OBJECT_STATEMENT_IDS);
+		return getValue(PredefinedStatementField.OBJECT_STATEMENT_IDS);
 	}
 	
 	public boolean hasModifiedSubject() {
-		return (getValue(StatementField.SUBJECT_STATEMENT_IDS) != null);
+		return (getValue(PredefinedStatementField.SUBJECT_STATEMENT_IDS) != null);
 	}
 
 	public boolean isProcessed() {
