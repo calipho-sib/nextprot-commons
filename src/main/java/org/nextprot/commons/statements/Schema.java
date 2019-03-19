cc03c22e7d47ed4857791f87b07fd18d612d42b3
fnikitin <frederic.nikitin@isb-sib.ch>
2019-03-19 18:13:01 +0100

    Move schema outside of NextProtSource

---
+++ src/main/java/org/nextprot/commons/statements/Schema.java
@@ -0,0 +1,37 @@
+package org.nextprot.commons.statements;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Schema {
+
+	private final Map<String, StatementField> statementFields = new HashMap<>();
+
+	public final void registerField(StatementField field) {
+
+		statementFields.put(field.getName(), field);
+	}
+
+	public int size() {
+		return statementFields.size();
+	}
+
+	public final boolean hasField(String field) {
+
+		return statementFields.containsKey(field);
+	}
+
+	public Collection<StatementField> getFields() {
+		return Collections.unmodifiableCollection(statementFields.values());
+	}
+
+	public StatementField getField(String field) {
+
+		if (hasField(field)) {
+			return statementFields.get(field);
+		}
+		throw new IllegalStateException("field "+ field + " is not valid");
+	}
+}
---
+++ src/test/java/org/nextprot/commons/statements/SchemaTest.java
@@ -0,0 +1,47 @@
+package org.nextprot.commons.statements;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class SchemaTest {
+
+	@Test
+	public void testRegisterField() {
+
+		Schema schema = new Schema();
+		schema.registerField(Mockito.mock(StatementField.class));
+		Assert.assertEquals(1, schema.size());
+	}
+
+	@Test
+	public void hasField() {
+
+		Schema schema = new Schema();
+		StatementField field = Mockito.mock(StatementField.class);
+		Mockito.when(field.getName()).thenReturn("roudoudou");
+		schema.registerField(field);
+		Assert.assertTrue(schema.hasField("roudoudou"));
+	}
+
+	@Test
+	public void getStatementFields() {
+
+		Schema schema = new Schema();
+		StatementField field = Mockito.mock(StatementField.class);
+		Mockito.when(field.getName()).thenReturn("roudoudou");
+		schema.registerField(field);
+		Assert.assertEquals(1, schema.getFields().size());
+		Assert.assertEquals("roudoudou", schema.getFields().iterator().next().getName());
+	}
+
+	@Test
+	public void getStatementField() {
+
+		Schema schema = new Schema();
+		StatementField field = Mockito.mock(StatementField.class);
+		Mockito.when(field.getName()).thenReturn("roudoudou");
+		schema.registerField(field);
+		Assert.assertEquals("roudoudou", schema.getField("roudoudou").getName());
+	}
+}
