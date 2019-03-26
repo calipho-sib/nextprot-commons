a425dcf03f623651a83f7aad3136205a48490d1c
fnikitin <frederic.nikitin@isb-sib.ch>
2019-03-26 11:40:08 +0100

    Add a new method + documentation

--- src/main/java/org/nextprot/commons/statements/schema/Schema.java
+++ src/main/java/org/nextprot/commons/statements/schema/Schema.java
@@ -1,13 +1,24 @@
 package org.nextprot.commons.statements.schema;
 
+import org.nextprot.commons.statements.CompositeField;
 import org.nextprot.commons.statements.StatementField;
 
 import java.util.Collection;
 
 public interface Schema {
 
-	StatementField getField(String field);
-	boolean hasField(String field);
+	/** @return the statement field named fieldName */
+	StatementField getField(String fieldName);
+
+	/** @return true schema has a field named fieldName */
+	boolean hasField(String fieldName);
+
+	/** @return the collection of statement fields */
 	Collection<StatementField> getFields();
+
+	/** @return the number of fields */
 	int size();
+
+	/** @return the composite field that contain the given field or null if absent */
+	CompositeField searchCompositeFieldOrNull(StatementField field);
 }
