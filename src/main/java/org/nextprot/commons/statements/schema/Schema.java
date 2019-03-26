ee1b0d6e7e92501afc365acb68d7f1cb3caefc91
fnikitin <frederic.nikitin@isb-sib.ch>
2019-03-26 17:42:31 +0100

    Add new method generateNXFlatTable

--- src/main/java/org/nextprot/commons/statements/schema/Schema.java
+++ src/main/java/org/nextprot/commons/statements/schema/Schema.java
@@ -1,9 +1,11 @@
 package org.nextprot.commons.statements.schema;
 
 import org.nextprot.commons.statements.CompositeField;
+import org.nextprot.commons.statements.GenericStatementField;
 import org.nextprot.commons.statements.StatementField;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public interface Schema {
 
@@ -21,4 +23,34 @@ public interface Schema {
 
 	/** @return the composite field that contain the given field or null if absent */
 	CompositeField searchCompositeFieldOrNull(StatementField field);
+
+	/** @return the sql to create the table schema for nxflat.{tableName} */
+	default String generateNXFlatTable(String tableName) {
+
+		StringBuilder sb = new StringBuilder();
+		sb.append("DROP TABLE IF EXISTS nxflat.").append(tableName).append(";\n");
+		sb.append("CREATE TABLE nxflat.").append(tableName).append(" (\n");
+
+		sb.append(getFields().stream()
+				.filter(StatementField::isNXFlatTableColumn)
+				.map(field -> "\t" + field.getName() + " VARCHAR(10000)")
+				.collect(Collectors.joining(",\n")));
+
+		sb.append(");\n");
+
+		sb.append("CREATE INDEX ").append(tableName, 0, 10)
+				.append("_ENTRY_AC_IDX ON nxflat.").append(tableName)
+				.append(" ( ")
+				.append(GenericStatementField.ENTRY_ACCESSION.name())
+				.append(" );\n");
+
+		sb.append("CREATE INDEX ").append(tableName, 0, 10)
+				.append("_ANNOT_ID_IDX ON nxflat.").append(tableName)
+				.append(" ( ")
+				.append(GenericStatementField.ANNOTATION_ID.name())
+				.append(" );\n");
+		sb.append("\n");
+
+		return sb.toString();
+	}
 }
--- src/test/java/org/nextprot/commons/statements/schema/SchemaTest.java
+++ src/test/java/org/nextprot/commons/statements/schema/SchemaTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 import org.nextprot.commons.statements.CompositeField;
 import org.nextprot.commons.statements.StatementField;
+import org.nextprot.commons.statements.constants.StatementTableNames;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -85,19 +86,71 @@ public class SchemaTest {
 		Assert.assertNull(cf);
 	}
 
+	@Test
+	public void testCreateGenericTable() {
+
+		String sql = new GenericSchema().generateNXFlatTable(StatementTableNames.ENTRY_TABLE);
+
+		Assert.assertEquals("DROP TABLE IF EXISTS nxflat.ENTRY_MAPPED_STATEMENTS;\n" +
+				"CREATE TABLE nxflat.ENTRY_MAPPED_STATEMENTS (\n" +
+				"\tANNOTATION_CATEGORY VARCHAR(10000),\n" +
+				"\tANNOTATION_ID VARCHAR(10000),\n" +
+				"\tANNOTATION_NAME VARCHAR(10000),\n" +
+				"\tANNOTATION_OBJECT_SPECIES VARCHAR(10000),\n" +
+				"\tANNOTATION_SUBJECT_SPECIES VARCHAR(10000),\n" +
+				"\tANNOT_CV_TERM_ACCESSION VARCHAR(10000),\n" +
+				"\tANNOT_CV_TERM_NAME VARCHAR(10000),\n" +
+				"\tANNOT_CV_TERM_TERMINOLOGY VARCHAR(10000),\n" +
+				"\tANNOT_DESCRIPTION VARCHAR(10000),\n" +
+				"\tANNOT_SOURCE_ACCESSION VARCHAR(10000),\n" +
+				"\tASSIGMENT_METHOD VARCHAR(10000),\n" +
+				"\tASSIGNED_BY VARCHAR(10000),\n" +
+				"\tBIOLOGICAL_OBJECT_ACCESSION VARCHAR(10000),\n" +
+				"\tBIOLOGICAL_OBJECT_DATABASE VARCHAR(10000),\n" +
+				"\tBIOLOGICAL_OBJECT_NAME VARCHAR(10000),\n" +
+				"\tBIOLOGICAL_OBJECT_TYPE VARCHAR(10000),\n" +
+				"\tDEBUG_INFO VARCHAR(10000),\n" +
+				"\tENTRY_ACCESSION VARCHAR(10000),\n" +
+				"\tEVIDENCE_CODE VARCHAR(10000),\n" +
+				"\tEVIDENCE_INTENSITY VARCHAR(10000),\n" +
+				"\tEVIDENCE_NOTE VARCHAR(10000),\n" +
+				"\tEVIDENCE_PROPERTIES VARCHAR(10000),\n" +
+				"\tEVIDENCE_QUALITY VARCHAR(10000),\n" +
+				"\tEVIDENCE_STATEMENT_REF VARCHAR(10000),\n" +
+				"\tEXP_CONTEXT_ECO_DETECT_METHOD VARCHAR(10000),\n" +
+				"\tEXP_CONTEXT_ECO_ISS VARCHAR(10000),\n" +
+				"\tEXP_CONTEXT_ECO_MUTATION VARCHAR(10000),\n" +
+				"\tGENE_NAME VARCHAR(10000),\n" +
+				"\tISOFORM_CANONICAL VARCHAR(10000),\n" +
+				"\tIS_NEGATIVE VARCHAR(10000),\n" +
+				"\tLOCATION_BEGIN VARCHAR(10000),\n" +
+				"\tLOCATION_BEGIN_MASTER VARCHAR(10000),\n" +
+				"\tLOCATION_END VARCHAR(10000),\n" +
+				"\tLOCATION_END_MASTER VARCHAR(10000),\n" +
+				"\tNEXTPROT_ACCESSION VARCHAR(10000),\n" +
+				"\tOBJECT_ANNOTATION_IDS VARCHAR(10000),\n" +
+				"\tOBJECT_ANNOT_ENTRY_UNAMES VARCHAR(10000),\n" +
+				"\tOBJECT_ANNOT_ISO_UNAMES VARCHAR(10000),\n" +
+				"\tOBJECT_STATEMENT_IDS VARCHAR(10000),\n" +
+				"\tRAW_STATEMENT_ID VARCHAR(10000),\n" +
+				"\tREFERENCE_ACCESSION VARCHAR(10000),\n" +
+				"\tREFERENCE_DATABASE VARCHAR(10000),\n" +
+				"\tRESOURCE_TYPE VARCHAR(10000),\n" +
+				"\tSOURCE VARCHAR(10000),\n" +
+				"\tSTATEMENT_ID VARCHAR(10000),\n" +
+				"\tSUBJECT_ANNOTATION_IDS VARCHAR(10000),\n" +
+				"\tSUBJECT_STATEMENT_IDS VARCHAR(10000),\n" +
+				"\tTARGET_ISOFORMS VARCHAR(10000),\n" +
+				"\tVARIANT_ORIGINAL_AMINO_ACID VARCHAR(10000),\n" +
+				"\tVARIANT_VARIATION_AMINO_ACID VARCHAR(10000));\n" +
+				"CREATE INDEX ENTRY_MAPP_ENTRY_AC_IDX ON nxflat.ENTRY_MAPPED_STATEMENTS ( ENTRY_ACCESSION );\n" +
+				"CREATE INDEX ENTRY_MAPP_ANNOT_ID_IDX ON nxflat.ENTRY_MAPPED_STATEMENTS ( ANNOTATION_ID );\n\n", sql);
+	}
+
 	private StatementField mockField(String name) {
 
 		StatementField field = Mockito.mock(StatementField.class);
 		Mockito.when(field.getName()).thenReturn(name);
-		return field;
-	}
-
-	private CompositeField mockCompositeField(String name, StatementField... fields) {
-
-		CompositeField field = Mockito.mock(CompositeField.class);
-		Mockito.when(field.getName()).thenReturn(name);
-
-
 		return field;
 	}
 }
