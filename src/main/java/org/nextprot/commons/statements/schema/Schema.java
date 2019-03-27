2e3103c7c6262c949fff1d5c84fa9f666886f538
fnikitin <frederic.nikitin@isb-sib.ch>
2019-03-27 14:52:06 +0100

    Move method to NxFlatTableSchema class

--- src/main/java/org/nextprot/commons/statements/schema/Schema.java
+++ src/main/java/org/nextprot/commons/statements/schema/Schema.java
@@ -29,34 +29,4 @@ public interface Schema {
 
 		return new JsonReader(this);
 	}
-
-	/** @return the sql to create the table schema for nxflat.{tableName} */
-	default String generateNXFlatTable(String tableName) {
-
-		StringBuilder sb = new StringBuilder();
-		sb.append("DROP TABLE IF EXISTS nxflat.").append(tableName).append(";\n");
-		sb.append("CREATE TABLE nxflat.").append(tableName).append(" (\n");
-
-		sb.append(getFields().stream()
-				.filter(StatementField::isNXFlatTableColumn)
-				.map(field -> "\t" + field.getName() + " VARCHAR(10000)")
-				.collect(Collectors.joining(",\n")));
-
-		sb.append(");\n");
-
-		sb.append("CREATE INDEX ").append(tableName, 0, 10)
-				.append("_ENTRY_AC_IDX ON nxflat.").append(tableName)
-				.append(" ( ")
-				.append(GenericStatementField.ENTRY_ACCESSION.name())
-				.append(" );\n");
-
-		sb.append("CREATE INDEX ").append(tableName, 0, 10)
-				.append("_ANNOT_ID_IDX ON nxflat.").append(tableName)
-				.append(" ( ")
-				.append(GenericStatementField.ANNOTATION_ID.name())
-				.append(" );\n");
-		sb.append("\n");
-
-		return sb.toString();
-	}
 }
